adca495cee33fcd88e56f8c23154275a70de1708
sufulu <1760473022@qq.com>
2017-07-06 14:13:41 +0800

    Initial commit

---
+++ baselibrary/src/main/java/com/linksu/fast/coding/baselibrary/BaseActivity.java
@@ -0,0 +1,27 @@
+package com.linksu.fast.coding.baselibrary;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * ================================================
+ * 作    者：linksus
+ * 版    本：1.0
+ * 创建日期：7/6 0006
+ * 描    述：All is Acitvity extends BaseActivity
+ * 修订历史：
+ * ================================================
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
